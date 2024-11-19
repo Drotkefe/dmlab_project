69e916f826daaa15c271f7b975c48ba1e2e0edd1
drotkefe <drotkefe@stud.uni-obuda.hu>
2024-11-19 16:03:30 +0100

    db commit

--- collect_data.py
+++ collect_data.py
@@ -2,11 +2,13 @@ import bs4
 import requests
 import sqlite3
 import pandas as pd
+import re
+import time
 
 URL = "https://www.autoscout24.hu/lst?sort=standard&desc=0&ustate=N%2CU&atype=C&source=homepage_search-mask"
 
 class Car:
-    def __init__(self,brand,model,fuel_type,year,horse_power,milage,transmission,price):
+    def __init__(self,brand='',model='',fuel_type='',year=0,horse_power=0,milage=0,transmission='',price=0):
         self.brand=brand
         self.model=model
         self.fuel_type=fuel_type
@@ -15,26 +17,26 @@ class Car:
         self.milage=milage
         self.transmission=transmission
         self.price=price
-
-    def __init__(self):
-        pass
+        self.hash=hash(self)
 
     @staticmethod
-    def read_hp(s: bs4.element):
-        pass
+    def read_hp(s: str):
+        try:
+            hp=re.findall(r'\d+',s)[-1]
+            return hp
+        except:
+            return 0
+        
+    def __eq__(self, other):
+        return isinstance(other,Car) and self.brand==other.brand and self.model==other.model and self.fuel_type==self.fuel_type and self.horse_power==other.horse_power and self.milage==other.milage and self.model==other.price and self.year==other.year and self.transmission==self.transmission
 
-    @staticmethod
-    def read_transmission(s: bs4.element):
-        pass
-    
-    @staticmethod
-    def read_fuel_type(s: bs4.element):
-        pass
+    def __hash__(self):
+        return hash((self.price,self.milage,self.year))
 
 def init_db():
     connection = sqlite3.connect("cars_data.db")
     cursor = connection.cursor()
-    cursor.execute(''' CREATE TABLE cars( 
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS cars( 
                   BRAND TEXT NOT NULL,
                   MODEL TEXT NOT NULL,
                   FUEL_TYPE TEXT NOT NULL,
@@ -42,34 +44,47 @@ def init_db():
                   HORSE_POWER INT NOT NULL,
                   MILAGE INT NOT NULL,
                   TRANSMISSION TEXT NOT NULL,
-                  PRICE INT NOT NULL);
+                  PRICE INT NOT NULL,
+                  HASH TEXT UNIQUE NOT NULL);
               ''')
     return cursor
 
 def get_cars():
     cars=[]
     result = requests.get(URL)
-    soup = bs4.BeautifulSoup(result.text,"html.parser")
+    soup = bs4.BeautifulSoup(result.text,'html.parser')
     pages=1
-    while len(cars)<2000:
+    while pages<21:
         for row in soup.find_all('article'):
             new_car=Car()
             new_car.brand=row['data-make']
             new_car.model=row['data-model']
             new_car.fuel_type=row.find('span', {'data-testid' : 'VehicleDetails-gas_pump'}).text
             new_car.year=row['data-first-registration'][-4:]
-            new_car.horse_power=new_car.read_hp("alma")
-            new_car.milage=row['data-milage']
-            new_car.transmission=row['']
+            new_car.horse_power=new_car.read_hp(row.find('span', {'data-testid' : 'VehicleDetails-speedometer'}).text)
+            new_car.milage=row['data-mileage']
+            new_car.transmission=row.find('span', {'data-testid' : 'VehicleDetails-transmission'}).text
             new_car.price=row['data-price']
             cars.append(new_car)
-            print(row['data-price'])
-            print(row['data-make'])
         pages+=1
-        result= requests.get(f"https://www.autoscout24.hu/lst?atype=C&desc=0&page={pages}&search_id=yui93b7btz&sort=standard&source=listpage_pagination&ustate=N%2CU")
+        result= requests.get(f'https://www.autoscout24.hu/lst?atype=C&desc=0&page={pages}&search_id=16eobrdiv6y&sort=standard&source=listpage_pagination&ustate=N%2CU')
+        soup = bs4.BeautifulSoup(result.text,'html.parser')
+    return cars
 
-    
+def insert_data_to_db(data,db):
+    for car in data:
+        db.execute('''INSERT OR IGNORE INTO cars (brand,model,fuel_type,year,horse_power,milage,transmission,price,hash) 
+                   VALUES (?,?,?,?,?,?,?,?,?)''',(car.brand,car.model,car.fuel_type,car.year,car.horse_power,car.milage,car.transmission,car.price,car.hash))
+    db.execute('SELECT * FROM cars')
+    d=db.fetchall()
+    print()
+
 
 if __name__ == "__main__":
-    # db=init_db()
-    get_cars()
+    search_ids=[
+       '16eobrdiv6y','yui93b7btz'  
+    ]
+    db=init_db()
+    cars=get_cars()
+    insert_data_to_db(cars,db)
+    
